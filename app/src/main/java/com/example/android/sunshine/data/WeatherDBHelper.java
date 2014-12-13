db297570f91e6f4dbf9c22531d0276dbbe21679d
Fer <pher.gm@gmail.com>
2014-12-13 17:21:40 +0100

    Location Table creation added to WeatherDBHelper

--- app/src/main/java/com/example/android/sunshine/data/WeatherDBHelper.java
+++ app/src/main/java/com/example/android/sunshine/data/WeatherDBHelper.java
@@ -28,6 +28,15 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
 
         // TBD
 
+        final String SQL_CREATE_LOCATION_TABLE = "CREATE TABLE " + LocationEntry.TABLE_NAME + " (" +
+                LocationEntry._ID + " INTEGER PRIMARY KEY," +
+                LocationEntry.COLUMN_LOCATION_SETTING + " TEXT UNIQUE NOT NULL, " +
+                LocationEntry.COLUMN_CITY_NAME + " TEXT NOT NULL, " +
+                LocationEntry.COLUMN_COORD_LAT + " REAL NOT NULL, " +
+                LocationEntry.COLUMN_COORD_LONG + " REAL NOT NULL, " +
+                "UNIQUE (" + LocationEntry.COLUMN_LOCATION_SETTING +") ON CONFLICT IGNORE"+
+                " );";
+
         final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherEntry.TABLE_NAME + " (" +
                 // Why AutoIncrement here, and not above?
                 // Unique keys will be auto-generated in either case.  But for weather
